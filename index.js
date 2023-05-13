41c6de698a51a2d1fa34f5433017555f07ba5f69
Roberto Morales <roberto@morales.com.co>
2023-05-13 11:42:39 -0500

    Tipos datos, conversiones, comparaciones entre tipos y errores de javascript

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ Comentarios de más de una línea
 let a = 5;
 let b = 6;
 let suma = a + b;
-console.log("El resultado de la suma es "+suma)
+//console.log("El resultado de la suma es "+suma)
 
 let nombre = "Roberto"
 let nombre_dos = nombre
 
 nombre_dos = "Carlos"
-console.log("El primer nombre es: ", nombre)
-console.log("El segundo nombre es: ",nombre_dos)
+//console.log("El primer nombre es: ", nombre)
+//console.log("El segundo nombre es: ",nombre_dos)
 
 // Notaciones de variables
 
@@ -45,4 +45,141 @@ let nombre_completo = "Roberto Morales Ortega"
 const PI = 3.1416;
 const COLOR = "Rojo";
 
+// Tipos de datos
 
+// Tipo de dato Number
+
+let n = 123;
+//console.log(n);
+
+// Valores numéricos especiales, Infinity y NaN
+
+//console.log(1 / 0)
+
+// Nan representa un error de cálculo, pero no es un error en javascript
+
+//console.log("texto" / 2);
+
+// NaN es pegajoso. Cualquier operación con NaN devuelve NaN
+//console.log("texto" - 2 * 5)
+
+// Tipo String
+
+let t = 'a'
+let c = "a"
+let d = `a` // Backtiks funcionad extendida
+//console.log('Roberto alias "Robert"')
+//console.log("I can't do it")
+//console.log(`Hola Mundo`)
+
+nombre = "Roberto";
+//console.log(`Hola ${nombre}`)
+//console.log('Hola ${nombre}')
+//console.log("Hola ${nombre}")
+
+// Tipo Booleano
+// true y false
+
+let verdadero = true
+let falso = false
+let mayor = 4 > 5
+
+// Valor null
+
+// Valor especial que NO PERTENECE A NINGUN TIPO DE DATO ANTERIOR.
+// Forma un tipo propio separado que contiene sólo el valor de null
+
+let edad = null;
+//console.log(edad)
+
+// En JS null NO SIGNIFICA UN OBJETO NO EXISTENTE
+// EN JS null es un valor especial que significa nada, vacío o desconocido
+
+// Valor Undefined
+
+// Valor especial que NO PERTENECE A NINGUN TIPO DE DATO ANTERIOR.
+// Undefined significa valor no asignado
+
+let valor_no_asignado;
+//console.log(valor_no_asignado);
+
+// Operador typeof o función typeof(x)
+// devuelte un tipo de dato
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(0)) // number
+console.log(typeof(true)) // boolean
+console.log(typeof(3.1416)) // number
+console.log(typeof('Hola Mundo')) //string
+
+console.log(typeof(alert)) // function
+console.log(typeof(null)) // Object - ERROR DE JAVASCRIPT
+
+
+// CONVERSIÓN DE TIPOS
+
+// Convertir a String
+let valor = true;
+valor = String(valor); // "true"
+valor = String(false) // "false"
+valor = String(null) // "null"
+valor = String(undefined) //"undefined"
+
+// Conversión a Number
+
+// Conversiones numéricas ocurren automáticamente en funciones 
+// matemáticas y expresiones.
+
+console.log('6' / '2') // 3
+console.log(1 + '2') // '12' + es concatenación
+console.log('1' + 2) // '12' + es concatenación
+
+valor = Number(undefined) // NaN
+valor = Number(null) // 0
+valor = Number(true) // 1
+valor = Number(false) // 0
+valor = Number('') // 0
+valor = Number('Hola Mundo') //NaN
+console.log(valor) 
+
+// Conversión a Boolean
+
+valor = Boolean(undefined) // false
+valor = Boolean(null) // false
+valor = Boolean('') // false
+valor = Boolean(NaN) // false
+valor = Boolean(0) // false
+valor = Boolean('0') // true
+valor = Boolean('hola') // true
+
+/*
+TABLA CONVERSIÓN NÚMEROS
+undefined -> NaN
+null -> 0
+true -> 1
+false -> 0
+string '' -> 0
+string 'hola' -> NaN
+*/
+
+/*
+TABLA CONVERSIÓN BOOLEANOS
+undefined -> false
+null -> false
+string '' -> false
+NaN -> false
+0 -> false
+
+'0' -> true
+'  ' -> true
+*/
+
+// Comparaciones entre diferente tipos
+console.log('2'>1) // true
+console.log('01' == 1) // true
+console.log(true > 0) // true
+console.log(false > 0) //false
+console.log('hola'>1) // false
+console.log(null == undefined) // true Error de JavaScript
+console.log(0 == false) // true
+console.log('0' == false) // true
