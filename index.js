4e0119f1ec383aed81f6057eda42bbba801e359a
Roberto Morales <roberto@morales.com.co>
2023-05-13 10:19:54 -0500

    comentarios y variables

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-alert('Hola Mundo');
+// Hola Mundo
+
+//alert('Hola Mundo');
+
+// Comentarios de línea
+/*
+Comentarios de más de una línea
+*/
+
+// Sentencias
+//alert('Hola'); alert('Mundo');
+
+//alert('Hola')
+//alert('Mundo')
+
+// Variables
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es "+suma)
+
+let nombre = "Roberto"
+let nombre_dos = nombre
+
+nombre_dos = "Carlos"
+console.log("El primer nombre es: ", nombre)
+console.log("El segundo nombre es: ",nombre_dos)
+
+// Notaciones de variables
+
+camelCase = 5
+snake_case = 5
+Pascal_Case = 5
+
+
+let nombre_completo = "Roberto Morales Ortega"
+
+// No puedes iniciar con número
+// No puede tener guión medio
+
+
+// Constantes
+
+const PI = 3.1416;
+const COLOR = "Rojo";
+
+
